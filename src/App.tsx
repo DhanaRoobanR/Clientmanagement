92924cef47688233ef2af7d4e892d5815e8dbf79
DhanaRoobanR <dhanarooban@softworthsolutions.in>
2025-05-13 11:04:57 +0530

    Added-matrial-ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React + TypeScript!</h1>
+      <Button variant="contained">Click Me</Button>
+    </div>
+  );
+}
+
+export default App;
